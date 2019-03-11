055e74c72fca3aaf6ab14b47fc7460fb08abeae3
Raymond Loman <raymond@carpago.nl>
2019-03-11 11:11:51 +0100

    Some changes which might be obosolte

---
+++ app/src/androidTest/java/nl/carpago/numbertheoryapp/EmailValidatorTest.java
@@ -0,0 +1,12 @@
+package nl.carpago.numbertheoryapp;
+
+
+import org.junit.runner.RunWith;
+import android.support.test.runner.*;
+
+@RunWith(AndroidJUnit4.class)
+public class EmailValidatorTest {
+
+
+
+}
