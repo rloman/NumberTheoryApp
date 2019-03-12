585824cb55b4dbee26a765db71a514c22fe86d6b
Raymond Loman <raymond@carpago.nl>
2019-03-12 21:21:24 +0100

    Stage this for continue later

--- app/src/androidTest/java/nl/carpago/numbertheoryapp/EmailValidatorTest.java
+++ app/src/androidTest/java/nl/carpago/numbertheoryapp/EmailValidatorTest.java
@@ -1,11 +1,17 @@
 package nl.carpago.numbertheoryapp;
 
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import android.support.test.runner.*;
 
 @RunWith(AndroidJUnit4.class)
 public class EmailValidatorTest {
+
+    @Test
+    public void testEvent() {
+        int scenario = launchActivity<MainActivity>();
+    }
 
 
 
